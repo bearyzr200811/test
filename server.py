85400a4c087c923d13d197ec6bf30e26f92138ab
bearyzr200811 <yzr200811@gmail.com>
2018-09-22 17:10:57 -0700

    full test

--- server.py
+++ server.py
@@ -7,13 +7,14 @@ from doc2vec import *
 import sys
 
 app = Flask(__name__)
-'''
+
 @app.route("/article/<topic>/<filename>")
 def article(topic,filename):
     """
     Show an article with relative path filename. Assumes the BBC structure of
     topic/filename.txt so our URLs follow that.
     """
+    
     for a in articles_list:
         if a[0]==topic+'/'+filename:
             title = a[1]
@@ -24,19 +25,19 @@ def article(topic,filename):
     results = recommended(doc, articles_list, 5)
     
     return render_template('article.html', title=title, content=content, results=results)
- ''' 
+  
 @app.route("/")
 def articles():
     """Show a list of article titles"""
-    #return render_template('articles.html', articles=articles_list)
-    return "hello" 
+    return render_template('articles.html', articles=articles_list)
+    #return "hello" 
 # initialization
-#i = sys.argv.index('server:app')
-#glove_filename = sys.argv[i+1]
-#articles_dirname = sys.argv[i+2]
+i = sys.argv.index('server:app')
+glove_filename = sys.argv[i+1]
+articles_dirname = sys.argv[i+2]
 
-#gloves = load_glove(glove_filename)
-#articles_list = load_articles(articles_dirname, gloves)
+gloves = load_glove(glove_filename)
+articles_list = load_articles(articles_dirname, gloves)
 
 
 
